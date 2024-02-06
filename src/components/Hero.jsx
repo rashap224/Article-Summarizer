f82e1bd24cc6114f978dc098235a99a1cfe57ef3
rashap224 <rashapofficial11@gmail.com>
2024-02-06 18:51:15 +0530

    Initial changes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/rashap224", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+      Transforming lengthy articles into succinct insights, Sumz is an AI-driven web app that revolutionizes content consumption with intelligent and swift summarization
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
